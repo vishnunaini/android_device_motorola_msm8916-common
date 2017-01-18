fc15c118a61295f556ff76c102e1ae228ab6f322
Vishnu Vardhan Reddy Naini <vishnu@vishnunaini.com>
2017-01-18 11:53:46 +0530

    yama:disable unpriviliged ptrace by default
    Change-Id: I30c8995bf8e65366b7155c84e793fcebd66acc3d

--- msm8916.mk
+++ msm8916.mk
@@ -203,6 +203,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
+ifeq ($(user_variant),user)
+    ADDITIONAL_DEFAULT_PROPERTIES += security.ptrace_scope=2
+endif
+
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
